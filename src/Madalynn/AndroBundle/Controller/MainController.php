7dd69ccc07d71aa474a21f3348cd927bb9359f55
Julien Brochet <mewt@madalynn.eu>
2011-07-29 19:45:34 +0200

    added abstract Contact page

--- src/Madalynn/AndroBundle/Controller/MainController.php
+++ src/Madalynn/AndroBundle/Controller/MainController.php
@@ -13,11 +13,36 @@
 namespace Madalynn\AndroBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+use Madalynn\AndroBundle\Entity\Contact;
+use Madalynn\AndroBundle\Form\ContactType;
 
 class MainController extends Controller
 {
     public function homepageAction()
     {
         return $this->render('AndroBundle:Main:homepage.html.twig');
+    }
+
+    public function contactAction(Request $request)
+    {
+        $contact = new Contact();
+        $form = $this->createForm(new ContactType(), $contact);
+
+        if ($request->getMethod() == 'POST') {
+            $form->bindRequest($request);
+
+            if ($form->isValid()) {
+                // Send mail later ...
+
+                $this->get('session')->setFlash('notice', 'Your message has been sent!');
+                $form->setData(new Contact());
+            }
+        }
+
+        return $this->render('AndroBundle:Main:contact.html.twig', array(
+            'form' => $form->createView()
+        ));
     }
 }
---
+++ src/Madalynn/AndroBundle/Form/ContactType.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * This file is part of the AndroIRC website.
+ *
+ * (c) 2010-2011 Julien Brochet <mewt@androirc.com>
+ * (c) 2010-2011 Sébastien Brochet <blinkseb@androirc.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Madalynn\AndroBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilder;
+
+class ContactType extends AbstractType
+{
+    public function buildForm(FormBuilder $builder, array $options)
+    {
+        $builder->add('email');
+        $builder->add('subject');
+        $builder->add('content', 'textarea');
+    }
+
+    public function getDefaultOptions(array $options)
+    {
+        return array(
+            'data_class' => 'Madalynn\AndroBundle\Entity\Contact'
+        );
+    }
+
+    public function getName()
+    {
+        return 'contact';
+    }
+}
