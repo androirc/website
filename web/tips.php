b9b04f785434b89df987d1e99bd79e6ceb7e7b80
Julien Brochet <mewt@madalynn.eu>
2011-09-02 21:22:45 +0200

    added tips.php

---
+++ web/tips.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * This file is part of the AndroIRC website.
+ *
+ * (c) 2010-2011 Julien Brochet <mewt@androirc.com>
+ * (c) 2010-2011 Sébastien Brochet <blinkseb@androirc.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+$lang = get('lang', 'en');
+$date = get('date');
+
+$url = 'http://www.androirc.com/tip/' . $lang;
+
+if ($date) {
+    $url .= '/' . $date;
+}
+
+header('Status: 301 Moved Permanently', false, 301);
+header('Location: ' . $url);
+
+function get($name, $default = null)
+{
+    return isset($_GET[$name]) ? $_GET[$name] : $default;
+}
