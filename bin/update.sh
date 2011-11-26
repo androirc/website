f6285c512c786ec8b0eac689fa82a4b41a4e68ba
Julien Brochet <mewt@madalynn.eu>
2011-11-26 17:37:43 +0100

    fixed typo

--- bin/update.sh
+++ bin/update.sh
@@ -6,5 +6,5 @@ php app/console assets:install --symlink web
 echo "assetic:dump :"
 php app/console assetic:dump --env=prod --no-debug
 
-echo "clear:cache :"
-php app/console clear:cache --env=prod --no-debug
+echo "cache:clear :"
+php app/console cache:clear --env=prod --no-debug
