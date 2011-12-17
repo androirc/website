7bde48a5b34f4a4d4e957b3d072fbc8eb81c5644
Julien Brochet <mewt@madalynn.eu>
2011-12-17 19:06:52 +0100

    added testDonate

--- src/Madalynn/AndroBundle/Tests/Controller/MainControllerTest.php
+++ src/Madalynn/AndroBundle/Tests/Controller/MainControllerTest.php
@@ -12,7 +12,15 @@ class MainControllerTest extends WebTestCase
         $crawler = $client->request('GET', '/');
 
         $this->assertTrue($client->getResponse()->isSuccessful());
-
         $this->assertEquals(5, $crawler->filter('.article')->count());
+    }
+
+    public function testDonate()
+    {
+        $client = self::createClient();
+        $crawler = $client->request('GET', '/donate');
+
+        $this->assertTrue($client->getResponse()->isSuccessful());
+        $this->assertEquals(2, $crawler->filter('#content ul > li')->count());
     }
 }
