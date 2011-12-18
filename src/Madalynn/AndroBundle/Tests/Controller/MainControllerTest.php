df1b3a6694c23842e615d01bc407ddd62b8bd8de
Julien Brochet <mewt@madalynn.eu>
2011-12-18 21:44:18 +0100

    fixed mobileHeaderTest

--- src/Madalynn/AndroBundle/Tests/Controller/MainControllerTest.php
+++ src/Madalynn/AndroBundle/Tests/Controller/MainControllerTest.php
@@ -35,20 +35,10 @@ class MainControllerTest extends WebTestCase
     public function testMobileHeader()
     {
         $client = self::createClient();
-        $useragents = array(
-            array('Mozilla/5.0 (Linux; U; Android 2.2; en-us; Nexus One Build/FRF91) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1', true),
-            array('Mozilla/5.0 (Linux; U; Android 2.1-update1; de-de; HTC Desire 1.19.161.5 Build/ERE27) AppleWebKit/530.17 (KHTML, like Gecko) Version/4.0 Mobile Safari/530.17', true),
-            array('Mozilla/5.0 (Windows; U; Windows NT 6.1; fr; rv:1.9.2) Gecko/20100115 Firefox/3.6', false),
-            array('Mozilla/5.0 (Windows NT 6.1) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/14.0.835.186 Safari/535.1', false),
-            array('Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; Trident/5.0; SLCC1; .NET CLR 2.0.50727; Media Center PC 5.0; .NET CLR 3.5.30729; .NET CLR 3.0.30729; OfficeLiveConnector.1.5; OfficeLivePatch.1.3; .NET4.0C)', false)
-        );
 
-        foreach ($useragents as $useragent) {
-            $client->request('GET', '/', array(), array(), array(
-                'HTTP_USER_AGENT' => $useragent[0]
-            ));
-
-            //$this->assertEquals($useragent[1], $client->getRequest()->headers->has('X-AndroIRC-Mobile'));
+        foreach (array('m' => true, 'www' => false) as $key => $value) {
+            $client->request('GET', sprintf('http://%s.androirc.com/', $key));
+            $this->assertEquals($value, $client->getRequest()->headers->has('X-AndroIRC-Mobile'));
         }
     }
 }
