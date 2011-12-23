518e242d706ec904c9a0acbda15485d657ba6366
Julien Brochet <mewt@madalynn.eu>
2011-12-24 00:42:00 +0100

    fixed bug on beta upload

--- src/Madalynn/Bundle/AndroBundle/Entity/BetaRelease.php
+++ src/Madalynn/Bundle/AndroBundle/Entity/BetaRelease.php
@@ -254,7 +254,7 @@ class BetaRelease
 
     protected function getUploadRootDir()
     {
-        return __DIR__ . '/../../../../web/' . $this->getUploadDir();
+        return __DIR__ . '/../../../../../web/' . $this->getUploadDir();
     }
 
     protected function getUploadDir()
