948ef5047923f24db7262901d61529b244a04648
Julien Brochet <mewt@madalynn.eu>
2011-12-24 19:10:14 +0100

    used PlumBundle instead of DeployBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -38,7 +38,7 @@ class AppKernel extends Kernel
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
             $bundles[] = new JMS\DebuggingBundle\JMSDebuggingBundle($this);
-            $bundles[] = new Madalynn\Bundle\DeployBundle\MadalynnDeployBundle();
+            $bundles[] = new Madalynn\Bundle\PlumBundle\MadalynnPlumBundle();
         }
 
         return $bundles;
