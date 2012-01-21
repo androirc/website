cf55f757baf25b0d8f1d5fe800670aadad29d664
Julien Brochet <mewt@madalynn.eu>
2012-01-21 22:14:05 +0100

    added composer.json

--- app/autoload.php
+++ app/autoload.php
@@ -1,62 +1,21 @@
 <?php
 
-/*
- * This file is part of the AndroIRC website.
- *
- * (c) 2010-2011 Julien Brochet <mewt@androirc.com>
- * (c) 2010-2011 Sébastien Brochet <blinkseb@androirc.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
-use Symfony\Component\ClassLoader\UniversalClassLoader;
 use Doctrine\Common\Annotations\AnnotationRegistry;
 
-$loader = new UniversalClassLoader();
-$loader->registerNamespaces(array(
-    'Symfony'                        => array(__DIR__.'/../vendor/symfony/src', __DIR__.'/../vendor/bundles'),
-    'Madalynn'                       => array(__DIR__.'/../vendor/bundles', __DIR__.'/../src'),
-    'Doctrine\\Common\\DataFixtures' => __DIR__.'/../vendor/doctrine-fixtures/lib',
-    'Doctrine\\Common'               => __DIR__.'/../vendor/doctrine-common/lib',
-    'Doctrine\\DBAL'                 => __DIR__.'/../vendor/doctrine-dbal/lib',
-    'Doctrine'                       => __DIR__.'/../vendor/doctrine/lib',
-    'Assetic'                        => __DIR__.'/../vendor/assetic/src',
-    'Buzz'                           => __DIR__.'/../vendor/buzz/lib',
-    'Pagerfanta'                     => __DIR__.'/../vendor/pagerfanta/src',
-    'JMS'                            => __DIR__.'/../vendor/bundles',
-    'Ornicar'                        => __DIR__.'/../vendor/bundles',
-    'Bundle'                         => __DIR__.'/../vendor/bundles',
-    'Sensio'                         => __DIR__.'/../vendor/bundles',
-    'WhiteOctober'                   => __DIR__.'/../vendor/bundles',
-    'Faker'                          => __DIR__.'/../vendor/faker/src',
-    'Plum'                           => __DIR__.'/../vendor/plum/src',
-));
-$loader->registerPrefixes(array(
-    'Twig_Extensions_' => __DIR__.'/../vendor/twig-extensions/lib',
-    'Twig_'            => __DIR__.'/../vendor/twig/lib',
-));
+$loader = require_once __DIR__.'/../vendor/.composer/autoload.php';
 
 // intl
 if (!function_exists('intl_get_error_code')) {
-    require_once __DIR__.'/../vendor/symfony/src/Symfony/Component/Locale/Resources/stubs/functions.php';
-
-    $loader->registerPrefixFallbacks(array(__DIR__.'/../vendor/symfony/src/Symfony/Component/Locale/Resources/stubs'));
+    require_once __DIR__.'/../vendor/symfony/symfony/src/Symfony/Component/Locale/Resources/stubs/functions.php';
+    $loader->add(null, __DIR__.'/../vendor/symfony/symfony/src/Symfony/Component/Locale/Resources/stubs');
 }
-
-$loader->registerNamespaceFallbacks(array(
-    __DIR__.'/../src',
-));
-$loader->register();
 
 AnnotationRegistry::registerLoader(function($class) use ($loader) {
     $loader->loadClass($class);
     return class_exists($class, false);
 });
-AnnotationRegistry::registerFile(__DIR__.'/../vendor/doctrine/lib/Doctrine/ORM/Mapping/Driver/DoctrineAnnotations.php');
 
-// Swiftmailer needs a special autoloader to allow
-// the lazy loading of the init file (which is expensive)
-require_once __DIR__.'/../vendor/swiftmailer/lib/classes/Swift.php';
-Swift::registerAutoload(__DIR__.'/../vendor/swiftmailer/lib/swift_init.php');
-
+// Swiftmailer needs a special autoloader to
+// allow the lazy loading of the init file (which is expensive)
+require_once __DIR__.'/../vendor/swiftmailer/swiftmailer/lib/classes/Swift.php';
+Swift::registerAutoload(__DIR__.'/../vendor/swiftmailer/swiftmailer/lib/swift_init.php');
