ce925afe7f3d8278feab1f0c6011d07fddeb3abb
Julien Brochet <mewt@madalynn.eu>
2012-01-23 10:27:42 +0100

    Welcome to 2012

--- src/Madalynn/Bundle/AdmobBundle/DependencyInjection/Configuration.php
+++ src/Madalynn/Bundle/AdmobBundle/DependencyInjection/Configuration.php
@@ -3,8 +3,8 @@
 /*
  * This file is part of the AndroIRC website.
  *
- * (c) 2010-2011 Julien Brochet <mewt@androirc.com>
- * (c) 2010-2011 Sébastien Brochet <blinkseb@androirc.com>
+ * (c) 2010-2012 Julien Brochet <mewt@androirc.com>
+ * (c) 2010-2012 Sébastien Brochet <blinkseb@androirc.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
