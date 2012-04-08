de2819caaec9698ab82b5e2ebae74432bdb0f344
Julien Brochet <mewt@madalynn.eu>
2012-04-08 17:05:34 +0200

    added more tips tests

--- src/Madalynn/Bundle/AndroBundle/Tests/Controller/TipControllerTest.php
+++ src/Madalynn/Bundle/AndroBundle/Tests/Controller/TipControllerTest.php
@@ -6,17 +6,48 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class TipControllerTest extends WebTestCase
 {
-    public function testTipAction()
+    const NO_TIPS = 'No tips to display';
+
+    protected $client;
+
+    public function setUp()
     {
-        $client = self::createClient();
+        $this->client = self::createClient();
+    }
 
-        $crawler = $client->request('GET', '/tip/fr');
-        $this->assertNotEquals('', $client->getResponse()->getContent());
+    public function testWithLocale()
+    {
+        $this->client->request('GET', '/tip/fr');
+        $response = $this->client->getResponse();
 
-        $crawler = $client->request('GET', '/tip/es');
-        $this->assertEquals('No tips to display', $client->getResponse()->getContent());
+        $this->assertNotEmpty($response->getContent());
+        $this->assertNotEquals(self::NO_TIPS, $response->getContent());
+    }
 
-        $crawler = $client->request('GET', '/tip/en/2011-12-25');
-        $this->assertEquals('We wish you a merry christmas... *sings*', $client->getResponse()->getContent());
+    public function testWithoutLocale()
+    {
+        $this->client->request('GET', '/tip');
+        $this->assertEquals(404, $this->client->getResponse()->getStatusCode());
+    }
+
+    public function testMalformedDate()
+    {
+        $this->client->request('GET', '/tip/en/2012-13-12');
+        $response = $this->client->getResponse();
+
+        $this->assertNotEmpty($response->getContent());
+        $this->assertNotEquals(self::NO_TIPS, $response->getContent());
+    }
+
+    public function testUnknowLocale()
+    {
+        $this->client->request('GET', '/tip/es');
+        $this->assertEquals(self::NO_TIPS, $this->client->getResponse()->getContent());
+    }
+
+    public function testWithDate()
+    {
+        $this->client->request('GET', '/tip/en/2011-12-25');
+        $this->assertEquals('We wish you a merry christmas... *sings*', $this->client->getResponse()->getContent());
     }
 }
