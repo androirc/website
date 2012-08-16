50c581293839b457d1acf8423d8a7aaa0fd275f8
Julien Brochet <mewt@madalynn.eu>
2012-08-16 16:55:13 +0200

    splitted services XML files

--- src/Madalynn/Bundle/MainBundle/DependencyInjection/MainExtension.php
+++ src/Madalynn/Bundle/MainBundle/DependencyInjection/MainExtension.php
@@ -30,7 +30,10 @@ class MainExtension extends Extension
     public function load(array $configs, ContainerBuilder $container)
     {
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+
         $loader->load('services.xml');
+        $loader->load('listeners.xml');
+        $loader->load('templating.xml');
     }
 
     /**
