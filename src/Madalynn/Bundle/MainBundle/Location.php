291fea4e65b5c7af3ff986a3531643b78b9ca89b
Julien Brochet <mewt@madalynn.eu>
2012-08-28 22:06:58 +0200

    fixed geocoder behavior

--- src/Madalynn/Bundle/MainBundle/Location.php
+++ src/Madalynn/Bundle/MainBundle/Location.php
@@ -14,15 +14,35 @@ namespace Madalynn\Bundle\MainBundle;
 
 use Geocoder\Geocoder;
 
+/**
+ * Location service
+ *
+ * @author Julein Brochet <mewt@androirc.com>
+ */
 class Location
 {
+    /**
+     * @var \Geocoder\Geocoder
+     */
     private $geocoder;
 
+    /**
+     * Constructor
+     *
+     * @param Geocoder $geocoder A Geocoder instance
+     */
     public function __construct(Geocoder $geocoder)
     {
         $this->geocoder = $geocoder;
     }
 
+    /**
+     * Searchs a location from the user IP
+     *
+     * @param string $ip The client IP
+     *
+     * @return string The client country
+     */
     public function searchLocation($ip)
     {
         $informations = $this->geocoder->geocode($ip);
