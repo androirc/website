1c089340c796f8f43700d37c9a41763db366456c
Julien Brochet <mewt@madalynn.eu>
2012-09-13 14:39:35 +0200

    refactored version tests

--- src/Madalynn/Bundle/MainBundle/Tests/Entity/AndroircVersionTest.php
+++ src/Madalynn/Bundle/MainBundle/Tests/Entity/AndroircVersionTest.php
@@ -14,7 +14,7 @@ namespace Madalynn\Bundle\MainBundle\Tests\Entity;
 
 use Madalynn\Bundle\MainBundle\Entity\AndroircVersion;
 
-class VersionTest extends \PHPUnit_Framework_TestCase
+class AndroircVersionTest extends \PHPUnit_Framework_TestCase
 {
     /**
      * @dataProvider dataToStringRepresenation
@@ -42,24 +42,28 @@ class VersionTest extends \PHPUnit_Framework_TestCase
     /**
      * @dataProvider dataAccessors
      */
-    public function testAccessors($version, $major, $minor, $revision)
+    public function testAccessors($version, $major, $minor, $revision, $state)
     {
         $version = AndroircVersion::create($version);
 
         $this->assertEquals($major, $version->getMajor());
         $this->assertEquals($minor, $version->getMinor());
         $this->assertEquals($revision, $version->getRevision());
+        $this->assertEquals($state, $version->getState());
     }
 
     public function dataAccessors()
     {
         return array(
-            array('3.2.1', 3, 2, 1),
-            array('1.0', 1, 0, 0),
-            array('3', 3, 0, 0),
-            array('', 0, 0, 0),
-            array('foobar', 0, 0, 0),
-            array('4.2.2.2', 4, 2, 2),
+            array('3.2.1', 3, 2, 1, null),
+            array('1.0', 1, 0, 0, null),
+            array('3', 3, 0, 0, null),
+            array('', 0, 0, 0, null),
+            array('foobar', 0, 0, 0, null),
+            array('4.2.2.2', 4, 2, 2, null),
+            array('foo-beta-1', 0, 0, 0, 'beta-1'),
+            array('4.0-ALPHA-3', 4, 0, 0, 'ALPHA-3'),
+            array('4.1.2-RC1', 4, 1, 2, 'RC1'),
         );
     }
 }
