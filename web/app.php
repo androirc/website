4faffeb730dbb4e8f87de559efbfeb8fdd0616d8
Julien Brochet <mewt@madalynn.eu>
2012-11-08 00:23:03 +0100

    updated front php pages

--- web/app.php
+++ web/app.php
@@ -2,12 +2,21 @@
 
 //include "maintenance.php";
 
-require_once __DIR__.'/../app/bootstrap.php.cache';
-require_once __DIR__.'/../app/AppKernel.php';
-
+use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
+
+$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
+
+$loader = new ApcClassLoader('androirc', $loader);
+$loader->register(true);
+
+require_once __DIR__.'/../app/AppKernel.php';
 
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
 
-$kernel->handle(Request::createFromGlobals())->send();
+$request = Request::createFromGlobals();
+
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
