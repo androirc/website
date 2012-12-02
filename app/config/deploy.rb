e65d45e681239938572fafc2c804d6843f9efc6a
Julien Brochet <mewt@madalynn.eu>
2012-12-02 16:02:51 +0100

    updated deploy script

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -29,6 +29,6 @@ set :use_sudo,              false
 set :dump_assetic_assets,   true
 set :update_assets_version, true
 set :clear_controllers,     true
-set :permission_method,     :acl
+set :permission_method,     false
 
 before "symfony:bootstrap:build", "deploy:set_permissions"
